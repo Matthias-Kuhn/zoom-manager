8b5cfcd97e6d50937265b2fd7d00adc1d4a0e9ff
Matthias Kuhn <kuhn.matthias@protonmail.com>
2022-05-27 23:15:25 +0200

    implemented select-code

---
+++ src/main.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+// electron-store for data persistence
+const Store = require('electron-store')
+Store.initRenderer()
+
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+
+  console.log(app.getPath('userData'))
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, "images/zoommanagericon.png"),
+
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'select-screen.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+
+ipcMain.on('close-app', (evt, arg) => {
+  app.quit();
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/select-code.js
@@ -0,0 +1,70 @@
+class Meeting {
+    constructor(name, url, position) {
+        this.name = name;
+        this.url = url;
+        this.position = position;
+        this.name_selector = "#name"+position;
+        this.btn_selector = "#btn"+position;
+    }
+
+    isActive() {
+        return this.name != undefined && this.name.length > 0
+    }
+}
+
+(function() {
+    const { ipcRenderer } = require('electron')
+    const shell = require('electron').shell;
+
+    const Store = require('electron-store');
+    const store = new Store();
+
+
+    function init() {
+
+        // get meetings from store
+        let MEETINGS = [];
+        MEETINGS.push(new Meeting(store.get('meeting0.name'), store.get('meeting0.url'), '0'));
+        MEETINGS.push(new Meeting(store.get('meeting1.name'), store.get('meeting1.url'), '1'));
+        MEETINGS.push(new Meeting(store.get('meeting2.name'), store.get('meeting2.url'), '2'));
+        MEETINGS.push(new Meeting(store.get('meeting3.name'), store.get('meeting3.url'), '3'));
+        MEETINGS.push(new Meeting(store.get('meeting4.name'), store.get('meeting4.url'), '4'));
+        MEETINGS.push(new Meeting(store.get('meeting5.name'), store.get('meeting5.url'), '5'));
+        MEETINGS.push(new Meeting(store.get('meeting6.name'), store.get('meeting6.url'), '6'));
+        MEETINGS.push(new Meeting(store.get('meeting7.name'), store.get('meeting7.url'), '7'));
+
+        MEETINGS.forEach(meeting => {
+
+            // set text and button
+            if (meeting.isActive()) {
+                document.querySelector(meeting.name_selector).innerHTML = meeting.name
+                document.querySelector(meeting.btn_selector).src = 'src/images/zoombtn_normal.png'
+            }
+
+            // register click listeners
+            document.querySelector(meeting.btn_selector).addEventListener('click', function(e) {
+                if (meeting.isActive()) shell.openExternal(meeting.url)
+                else window.location.href = "src/input.html?"+meeting.position
+            });
+
+            // right click
+            document.querySelector(meeting.btn_selector).addEventListener('contextmenu', function(e) {
+                window.location.href = "src/input.html?"+meeting.position
+            });
+
+        })
+
+        document.querySelector('#exit-btn').addEventListener("click", function(e) {
+            ipcRenderer.send('close-me');
+        
+        });
+
+    }
+
+
+    document.onreadystatechange =  () => {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+})();
